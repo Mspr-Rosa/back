0620d92db815c27cc47e2a3af7ae71af05e72984
JulienDLTT <149057454+JulienDLTT@users.noreply.github.com>
2024-04-17 10:34:05 +0200

    Delete src directory

--- src/main/java/fr/espi/backmspr/backmspr/repository/FlowerRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.espi.backmspr.backmspr.repository;
-
-import fr.espi.backmspr.backmspr.entity.FlowerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlowerRepository extends JpaRepository<FlowerEntity, Long> {
-}
--- src/main/java/fr/espi/backmspr/backmspr/repository/FlowerTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.espi.backmspr.backmspr.repository;
-
-import fr.espi.backmspr.backmspr.entity.FlowerTypeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlowerTypeRepository extends JpaRepository<FlowerTypeEntity, Long> {
-}
